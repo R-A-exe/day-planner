56a4437176617ecbe3abd5c52a72df85b553faea
R-A-exe <royatalla.ra@gmail.com>
2020-12-06 20:24:48 -0500

    Optimizing script and html, reducing duplicate code and fixing bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var descriptionArea = $(".text-wrapper");
 var list = JSON.parse(localStorage.getItem("dayList"));
 
+// Update the current day on top of page
 function updateDay() {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 }
+
+//Load saved items, or clear saved items if first load of day
 function loadList() {
     if (list != null) {
         if (list[0].day == moment().format("MM-D-YY")) {
@@ -15,59 +17,40 @@ function loadList() {
             localStorage.setItem("dayList", "");
         }
     }
+
+//Update color coding
     var currentTime = moment().local().format("ha");
-    var present = false
-    $("textarea").each(function () {
-        if(moment().local().isBefore(moment("9am", "ha"))){
-            $(this).addClass("future");
-       } else if (currentTime != $(this).data("slot")) {
-            if(present){
-                $(this).addClass("future");
-            } else {
-                $(this).addClass("past");
-            }
-        } else {
-            $(this).addClass("present");
-            present = true;
+    var present = false;
+    $("textarea").each(function () {                            //For each text area
+        if(moment().local().isBefore(moment("9am", "ha"))){     //If it is before 9am
+            $(this).addClass("future");                         //Add class "future" (working day hasn't started yet)
+       } else if (currentTime != $(this).data("slot")) {        //If not, if the current time is not equal to current timeslot
+            if(present){                                        //And if loop has alreay reached the present timeslot
+                $(this).addClass("future");                     //Then add class "future"  
+            } else {                                            //If loop hasn't reached the present timeslot
+                $(this).addClass("past");                       //Then add class "past"
+            }                                                   
+        } else {                                                //If current time is equal to current timeslot
+            $(this).addClass("present");                        //Then add class "present"
+            present = true;                                     //And indicate that the loop already reached the present timeslot
         }
     });
 }
 
-function saveChanges(slot) {
-    var textarea = $("#" + slot + " textarea");
-    $(textarea).data("change", "");
-    $("#" + slot + " .save").empty();
-    var item = { day: moment().format("MM-D-YY"), time: $(textarea).data("slot"), agendaItem: $(textarea).val() }
-    if (list == null) {
-        list = [item];
-    } else {
-        list.push(item);
-    }
-    localStorage.setItem("dayList", JSON.stringify(list));
-}
-
-$("textarea").keyup(function () {
+//On change, add save button and its click listener for saving
+$("textarea").keydown(function () {
     var slot = $(this).data("slot");
-    $(this).data("change", "yes");
     if ($("#" + slot + " .save").html() == "") {
         $("#" + slot + " .save").append("<button class=\"saveBtn\" data-slot=\"" + slot + "\" >Save</button>");
-    }
-});
-
-$("textarea").blur(function () {
-    var slot = $(this).data("slot");
-    if ($(this).data("change") == "yes") {
-        var save = confirm("Would you like to save changes?");
-        if (save) {
-            saveChanges(slot);
-        } else {
-            $(this).data("change", "");
-            $(this).val("");
+        $(".saveBtn").click(function () {
+            var slot = $(this).data("slot");
             $("#" + slot + " .save").empty();
-        }
+            var item = { day: moment().format("MM-D-YY"), time: slot, agendaItem:  $("#" + slot + " textarea").val() }
+            list == null ? list = [item] : list.push(item);
+            localStorage.setItem("dayList", JSON.stringify(list));
+        });
     }
 });
-
 
 updateDay();
 loadList();
